1c580ed33bfdef964e59e88867920303c7636955
leeseohyun <applemint2003@gmail.com>
2023-03-27 12:00:44 +0900

    프로그래머스 문제풀이

---
+++ Java/객지프/prgrms/Prgrms_2IntSum.java
@@ -0,0 +1,24 @@
+package chap3;
+import java.util.Scanner;
+
+public class Prgrms_2IntSum {
+	
+	public static long solution(int a, int b) {
+        long answer = 0;
+        int x = (a<b)? a:b;
+        int y = (a<b)? b:a;
+        for(int i = x; i <= y; i++){
+            answer+=i;
+        }
+        return answer;
+    }
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int a = input.nextInt();
+		int b = input.nextInt();
+		System.out.println((long)solution(a,b));
+		input.close();
+	}
+
+}
