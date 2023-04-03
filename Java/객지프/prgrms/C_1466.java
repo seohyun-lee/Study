1fa3d240724457d5a150975e01643eb0bcd332b8
leeseohyun <applemint2003@gmail.com>
2023-04-03 20:13:10 +0900

    코드업 문제풀이
    2차원 배열, 메소드+배열

---
+++ Java/객지프/prgrms/C_1466.java
@@ -0,0 +1,23 @@
+package chap7;
+import java.util.Scanner;
+
+public class C_1466 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int cnt=n*m;
+		
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<m;j++) {
+				System.out.print(cnt+" ");
+				cnt-=n;
+			}
+			System.out.println();
+			cnt=n*m-i-1;
+		}
+		sc.close();
+	}
+
+}
---
+++ Java/객지프/prgrms/C_1568.java
@@ -0,0 +1,28 @@
+package chap8;
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class C_1568 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+
+		for(int i=0;i<n;i++) {
+			arr[i]=sc.nextInt();
+		}
+		int start = sc.nextInt();
+		int end = sc.nextInt();
+		int max=0;
+		int index=0;
+		for(int i=start-1;i<end;i++) {
+			if(arr[i]>max) {
+				max=arr[i];
+				index=i;
+			}
+		}
+		System.out.println(index+1);
+	}
+
+}
