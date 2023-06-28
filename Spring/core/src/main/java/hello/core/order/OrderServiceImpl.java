ad6b0124ba401e19df11c682cc03e4c96a1dcd2c
leeseohyun <applemint2003@gmail.com>
2023-06-28 16:51:59 +0900

    스프링 핵심 원리 - 기본편 (김영한)

--- Spring/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ Spring/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,16 +3,16 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 //import hello.core.member.MemoryMemberRepository; //->3장에서 AppConfig 사용해 필요없어짐
 
+@Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy; //3장에서 AppConfig 사용해 코드 변경함
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
